e2c250ebc264178377ee8df9c587dbb90ffed52b
LorenAlburquerque <lorenaalbuquerque.dev@gmail.com>
2025-05-07 19:46:39 -0300

    primeiro commit

--- aula-1/primeiro.js
+++ aula-1/primeiro.js
@@ -1,8 +1,13 @@
-console.log("Olá Mundo");
+// comentário de uma linha
 
-{
-    console.log("Olá Mundo");
-    console.log("Olá Mundo");
-}
+/* comentário de várias linhas */
 
-// e o caba vai endoidar é
+console.log("serve para imprimir algo na tela")
+
+{} //serve para criar um bloco de comando.
+
+// Outros:
+
+/* Node.js permite rodar o JS fora do navegador
+API é um Intermediário entre o servidor e o cliente
+protocolo - HTTP: permite a comunicação entre navegadores (clientes) e servidores web. */
