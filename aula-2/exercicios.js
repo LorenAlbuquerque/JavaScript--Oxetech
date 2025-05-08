5d8ba3613278dd620c9106d154ad79de3a942cf4
Lorena Albuquerque <109039857+LorenAlbuquerque@users.noreply.github.com>
2025-05-08 17:02:18 -0300

    Add files via upload

---
+++ aula-2/exercicios.js
@@ -0,0 +1,31 @@
+// 1 
+
+let nome;
+let idade;
+let altura;
+let genero;
+let estudante = true;
+
+nome = prompt ("seu nome:")
+idade  = prompt ("seu nome:")
+altura  = prompt ("seu nome:")
+genero = prompt ("seu genero")
+estudante = prompt("Vocês é uma estudante?");
+
+console.log ("nome: ", nome)
+console.log ("idade: ", idade)
+console.log ("altura: ", altura)
+console.log ("nome: ", nome)
+console.log ("genero: ", genero)
+console.log ("estudante: ", estudante)
+
+//2
+
+/*
+let compra;
+let desc10 = compra 
+let des5 = 5
+
+compra > 100 ? 
+
+compra = prompt ("Informe o valor da sua comprar?") */
---
+++ aula-2/segundo.js
@@ -0,0 +1,45 @@
+/* topos de variaveis no js LET - CONST
+
+VAR: espaço de armazenamento.
+tipos de VAR:
+
+primitivos:
+Number: inteiro e decimais
+Cadeia de textos: ('') ("")
+undefied: valor não atribuido.
+null: trabalando com variaveis complecias. ex: objetos etc */
+
+
+// Variáveis
+
+let nome = 'Lorena';
+let idade = 23;
+let altura = 1.64;
+let programador = true; 
+
+console.log ("nome: ", nome)
+console.log ("idade: ", idade)
+console.log ("altura: ", altura)
+console.log ("Programador: ", programador)
+
+//tipos de dados
+
+console.log ("nome: ", typeof(nome))
+console.log ("idade: ", typeof(idade))
+
+
+//Navegador
+
+//prompt:
+//alert() caxinha de texto 
+//console.log()
+
+
+/* let nome = 23
+
+${nome}
+
+console.log (nome_da_var)
+
+
+converte para number quando for usar prompt */
