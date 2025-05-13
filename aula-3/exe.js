110c8eb95882822ebf1d0195feb6d9d79e28f9df
Lorena Albuquerque <109039857+LorenAlbuquerque@users.noreply.github.com>
2025-05-13 16:49:31 -0300

    Add files via upload

---
+++ aula-3/exe.js
@@ -0,0 +1,18 @@
+let idade = parseInt(promt("Qual sua idade? "))
+
+if (idade >= 65) {
+    console.log("Vocêé um idoso.")
+}
+
+else if (idade >= 18) {
+    console.log("Você é um adulto.")
+}
+
+else if (idade >= 13) {
+    console.log("Você é uma criança.")
+}
+
+else {
+    console.log("Você é uma criança.")
+}
+
---
+++ aula-3/exer4.js
@@ -0,0 +1,9 @@
+let n1 = parseFloat(prompt("Digite o primeiro número: "))
+let n2 = parseFloat(prompt("Digite o segundo número: "))
+let escolha = parseInt
+console.log("--- Operações ---\n1 - soma\n2 - subtração\n3 - multiplicação\n4 - divisão\n-----------------")
+
+escolha = prompt("escolha uma operação")
+
+
+//usando o switch
